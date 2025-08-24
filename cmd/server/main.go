7e120f17a18847b3b3092f4954760661a7d54693
Sergey Kharchenko <skharchenko87@yandex.ru>
2025-08-24 20:33:41 +0300

    feat: заготовка main

---
+++ cmd/server/main.go
@@ -0,0 +1,75 @@
+// Package main точка входа
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	internalconfig "github.com/SKharchenko87/foodix/internal/config"
+	"github.com/SKharchenko87/foodix/pkg/config"
+	"github.com/SKharchenko87/foodix/pkg/logger"
+)
+
+type appArgs struct {
+	configPath string
+}
+
+func main() {
+	if err := runApp(); err != nil {
+		slog.Error("Application failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+func runApp() error {
+	// Создаем контекст, который отслеживает сигналы завершения
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	// Получаем аргументы запуска
+	args, err := parseArgs()
+	if err != nil {
+		return err
+	}
+
+	// Применяем конфигурацию
+	var cfg config.Config = internalconfig.NewYAMLConfig()
+	if err = cfg.Load(args.configPath); err != nil {
+		return err
+	}
+
+	// Инициализируем logger
+	log := logger.InitLogger(cfg.GetLogger())
+	log.InfoContext(ctx, "Starting application", "config_path", args.configPath)
+
+	// ToDo runServer(ctx, log)
+	// ToDo graceful shutdown.
+
+	log.InfoContext(ctx, "Application stopped successfully")
+	return nil
+}
+
+func parseArgs() (*appArgs, error) {
+	var pathConfig string // Получаем путь к файлу конфигурации
+	flgs := flag.NewFlagSet("server", flag.ExitOnError)
+	flgs.StringVar(&pathConfig, "config", "", "path to config file")
+
+	if err := flgs.Parse(os.Args[1:]); err != nil {
+		return nil, err
+	}
+
+	if pathConfig == "" {
+		return nil, flag.ErrHelp
+	}
+
+	return &appArgs{pathConfig}, nil
+}
+
+func runServer(ctx context.Context, log *slog.Logger) error {
+	// ToDo
+	return nil
+}
